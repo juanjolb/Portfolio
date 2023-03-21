70128b946f94ca070a77dc777321788f69a88c80
Juanjo Lozano <lbarcelo.juanjo@gmail.com>
2023-03-21 11:33:29 +0100

    routing and header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: 'home', component: HomeComponent },
-  // { path: '**', redirectTo: '/home' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AboutMeComponent },
+  { path: 'projects', component: AboutMeComponent },
+  { path: 'skills', component: AboutMeComponent },
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
